844797176a02b7aa295cf5b64672ac5a32dd8b7b
Unknown <adri_esp_11@hotmail.com>
2019-01-18 23:03:24 +0100

    feat: init

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 30;
+
+    void Update()
+    {
+        transform.position += transform.forward * _speed * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 bounds = new Vector3(75, 25, 125);
+
+    [SerializeField]
+    private GameObject _laserPrefab;
+
+    private Rigidbody _rigidbody;
+
+    private float _speed = 30;
+    private bool isMoving = false;
+    private float _moveX;
+    private float _moveY;
+    private float _moveZ;
+    private Vector3 _laserLeftPosition = new Vector3(-0.31f, 0.1f, 5.24f);
+    private Vector3 _laserRightPosition = new Vector3(0.31f, 0.1f, 5.24f);
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        
+    }
+
+    void Update()
+    {
+        _moveX = Input.GetAxis("Horizontal");
+        _moveY = Input.GetAxis("Vertical");
+        _moveZ = Input.GetAxis("Depth");
+
+        if (_moveX != 0 || _moveY != 0 || _moveZ != 0)
+        {
+            isMoving = true;
+        } else
+        {
+            isMoving = false;
+        }
+
+        Shoot();
+    }
+
+    void FixedUpdate()
+    {
+        if(isMoving)
+        {
+            Move();
+        }
+    }
+
+    void Move()
+    {
+        Vector3 direction = new Vector3(_moveX, _moveY, _moveZ);
+
+        _rigidbody.MovePosition(transform.position + (direction * _speed * Time.deltaTime));
+    }
+
+    void Shoot()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(_laserPrefab, transform.position + _laserLeftPosition, Quaternion.identity);
+            Instantiate(_laserPrefab, transform.position + _laserRightPosition, Quaternion.identity);
+        }
+    }
+}
